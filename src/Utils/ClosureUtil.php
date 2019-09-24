470f4b46f388161ceab136db794c8ad1f4274c34
沧澜 <lirongyi@51ucar.cn>
2019-09-24 16:31:03 +0800

    add check run

--- src/Utils/ClosureUtil.php
+++ src/Utils/ClosureUtil.php
@@ -37,6 +37,25 @@ class ClosureUtil
     }
     
     /**
+     * @param Closure|mixed $closure
+     * @param array $params
+     * @param mixed|null $default
+     * @return mixed
+     */
+    final public static function checkRun($closure, array $params = [], $default = null)
+    {
+        if ($closure instanceof Closure) {
+            return call_user_func_array($closure, $params);
+        } else if (is_array($closure) && count($closure) >= 2) {
+            return call_user_func_array(array_slice($closure, 0, 2), $params);
+        } else if (is_string($closure) && count(explode("::", $closure)) == 2) {
+            return self::checkRun(explode("::", $closure), $params);
+        } else {
+            return $default ?? $closure;
+        }
+    }
+    
+    /**
      * @param Closure|string|array $closure 检查的闭包函数或执行类(string/array)
      * @param array $params                 执行参数
      * @param null $default
