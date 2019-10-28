1c15510efd38fad763cd57261b68d77f8f043b98
沧澜 <lirongyi@51ucar.cn>
2019-10-28 19:02:50 +0800

    添加配置

--- src/Components/Comments/CallDataPropertyHeadComment.php
+++ src/Components/Comments/CallDataPropertyHeadComment.php
@@ -6,10 +6,14 @@
 
 namespace CalJect\Productivity\Components\Comments;
 
+use CalJect\Productivity\Components\Criteria\Criteria;
 use CalJect\Productivity\Contracts\Comments\ClassHeadComment;
 use CalJect\Productivity\Contracts\DataProperty\TCallDataProperty;
+use CalJect\Productivity\Contracts\DataProperty\TCallDataPropertyByName;
+use CalJect\Productivity\Exceptions\ClosureRunException;
 use CalJect\Productivity\Models\FileInfo;
 use CalJect\Productivity\Utils\CommentUtil;
+use Closure;
 use ReflectionClass;
 use ReflectionProperty;
 
@@ -17,28 +21,38 @@ use ReflectionProperty;
  * Class CallDataPropertyHeadComment
  * @package CalJect\Productivity\Components\Comments
  * ---------- set ----------
- * @method $this setTagNote(string $tagNote)    默认检查属性说明注释部分tag[@note]
- * @method $this setTagVar(string $tagVar)      默认检查值类型注释部分tag[@var]
- * @method $this setDefVar(string $defVar)      默认值类型
+ * @method $this setTagNote(string $tagNote)           默认检查属性说明注释部分tag
+ * @method $this setTagVar(string $tagVar)             默认检查值类型注释部分tag
+ * @method $this setDefVar(string $defVar)             默认值类型
+ * @method $this setClassCheck(Closure $classCheck)    类检查（检查是否生成注释）
+ * @method $this setOptions(int $options)              配置参数
 
  * ---------- get ----------
- * @method string getTagNote()    默认检查属性说明注释部分tag[@note]
- * @method string getTagVar()     默认检查值类型注释部分tag[@var]
- * @method string getDefVar()     默认值类型
+ * @method string  getTagNote()       默认检查属性说明注释部分tag
+ * @method string  getTagVar()        默认检查值类型注释部分tag
+ * @method string  getDefVar()        默认值类型
+ * @method Closure getClassCheck()    类检查（检查是否生成注释）
+ * @method int     getOptions()       配置参数
  */
 class CallDataPropertyHeadComment extends ClassHeadComment
 {
-    use TCallDataProperty;
+    use TCallDataProperty, TCallDataPropertyByName;
+    
+    const COM_SET = 1;          // 生成设置方法注释(get{$property}())
+    const COM_GET = 1 << 1;     // 生成获取方法注释(set{$property}(@var $propertyName))
+    const COM_APT = 1 << 2;     // 生成自动方法注释({property}(@var $propertyName = null))
     
     /**
-     * @note 默认检查属性说明注释部分tag[@note]
+     * @note 默认检查属性说明注释部分tag
      * @var string
+     * @expain 示例 note => @note
      */
     protected $tagNote = 'note';
     
     /**
-     * @note 默认检查值类型注释部分tag[@var]
+     * @note 默认检查值类型注释部分tag
      * @var string
+     * @expain 示例 var => @var
      */
     protected $tagVar = 'var';
     
@@ -49,10 +63,24 @@ class CallDataPropertyHeadComment extends ClassHeadComment
     protected $defVar = 'mixed';
     
     /**
+     * @note 类检查（检查是否生成注释）
+     * @var Closure
+     * @expain Closure 闭包响应bool值
+     */
+    protected $classCheck;
+    
+    /**
+     * @note 配置参数
+     * @var int
+     */
+    protected $options = 3;
+    
+    /**
      * @param FileInfo $fileInfo
      * @param ReflectionClass $refClass
      * @param string $filePath
      * @return string
+     * @throws ClosureRunException
      */
     protected function getComments(FileInfo $fileInfo, ReflectionClass $refClass, string $filePath): string
     {
@@ -70,16 +98,20 @@ class CallDataPropertyHeadComment extends ClassHeadComment
             $setStrMaxLen = ($strLen = strlen($setStr)) > $setStrMaxLen ? $strLen : $setStrMaxLen;
             $getStrMaxLen = ($strLen = strlen($getStr) - 4 + $varMaxLen) > $getStrMaxLen ? $strLen : $getStrMaxLen;
         }, $refClass->getProperties());
+        $options = Criteria::opts($this->options);
         if ($setStrMaxLen) {
-            $comment = " * ---------- set ----------\n";
-            foreach ($setting as $name => $str) {
-                $comment .= ($noting[$name] ? str_pad($str, $setStrMaxLen + 4, ' ') . $noting[$name] : $str) . "\n";
-            }
-            $comment .= "\n * ---------- get ----------\n";
-            foreach ($getting as $name => $content) {
-                $str = str_replace('$var', str_pad($content['var'], $varMaxLen, ' '), $content['str']);
-                $comment .= ($noting[$name] ? str_pad($str, $getStrMaxLen + 4, ' ') . $noting[$name] : $str) . "\n";
-            }
+            $options->bind(self::COM_SET, function () use ($setting, $noting, $setStrMaxLen, &$comment) {
+                $comment .= " * ---------- set ----------\n";
+                foreach ($setting as $name => $str) {
+                    $comment .= ($noting[$name] ? str_pad($str, $setStrMaxLen + 4, ' ') . $noting[$name] : $str) . "\n";
+                }
+            })->bind(self::COM_GET, function () use ($getting, $noting, $varMaxLen, $getStrMaxLen, &$comment) {
+                $comment .= "\n * ---------- get ----------\n";
+                foreach ($getting as $name => $content) {
+                    $str = str_replace('$var', str_pad($content['var'], $varMaxLen, ' '), $content['str']);
+                    $comment .= ($noting[$name] ? str_pad($str, $getStrMaxLen + 4, ' ') . $noting[$name] : $str) . "\n";
+                }
+            })->handle();
         }
         return $this->commentWithClassHead($fileInfo, $comment ?? '');
     }
---
+++ src/Contracts/DataProperty/TCallDataPropertyByName.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Author: 沧澜
+ * Date: 2019-10-28
+ */
+
+namespace CalJect\Productivity\Contracts\DataProperty;
+
+/**
+ * Trait TCallDataPropertyCallOther
+ * @package CalJect\Productivity\Contracts\DataProperty
+ * ---------- method ----------
+ * @method mixed __call($name, $arguments)
+ */
+trait TCallDataPropertyByName
+{
+    /**
+     * @param string $name
+     * @param $arguments
+     * @return mixed
+     */
+    protected function _callOther(string $name, $arguments)
+    {
+        if (property_exists($this, $name)) {
+            return $this->__call((isset($arguments[0]) ? 'set' : 'get') . ucfirst($name), $arguments);
+        }
+    }
+}
